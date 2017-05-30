2a78fb0cf1022a194bedaaadd7bd5dbb255be79e
Martin Haug <martinhaug@smart-mail.de>
2017-05-30 17:43:45 +0200

    + Kann Templates nutzen

--- test.py
+++ test.py
@@ -1,14 +1,19 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET"])
+@app.route("/index.html", methods=["GET"])
 def hello():
-    return "Heyho!"
+    return "Hey ho!"
 
+
+@app.route("/about")
+@app.route("/about.html")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
