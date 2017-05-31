3e72bc43d8cdd2536aaf45ca192dae1d81e68a93
Martin Haug <martinhaug@smart-mail.de>
2017-05-31 16:33:20 +0200

    + Kann neue Gruppe anlegen

--- DataModel.py
+++ DataModel.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-from enum import Enum
 
 import sqlalchemy
 from sqlalchemy import Integer, String, Column, ForeignKey, create_engine
@@ -14,15 +13,13 @@ def createDB(app):
     engine = create_engine('sqlite:////tmp/test.db', echo=False)
     SessionMaker = sessionmaker(bind=engine)
     session = SessionMaker()
+    Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
     return engine, session
 
-class _Status(Enum):
-    """Enum der festlegt in welchen Zuständen ein Testcase oder eine Gruppe sein kann"""
-    todo = 1 # Noch zu erledigen
-    implemented = 2 # Wurde implementiert, aber noch nicht an Microsens ausgeliefert
-    released = 3 # Wurde an Microsens ausgeliefert
 
+# Enum der festlegt in welchen Zuständen ein Testcase oder eine Gruppe sein kann
+stati = ["todo", "implemented", "released"]
 
 class Gruppe(Base):
     """Eine Gruppe von Testcases die gemeinsam ausgeführt wird und üblicherweise immer einen Namespace abdeckt"""
@@ -32,15 +29,14 @@ class Gruppe(Base):
     name = Column(String(120), unique=True, nullable=False, doc="Der Name dieser Gruppe")
     testcases = relationship("Testcase",
                              doc="Die zu dieser Gruppe gehörenden Testcases. Ein Testcase kann immer nur zu einer Gruppe gehören.")
-    status = Column(sqlalchemy.Enum("_Status"),
+    status = Column(sqlalchemy.Enum(*stati),
                     doc="Ob die Gruppe bereits implementiert/released wurde oder ob dies noch ein TODO ist")
-
 
 class Testcase(Base):
     __tablename__ = 'testcases'
     tid = Column(Integer, primary_key=True)
     name = Column(String(120), nullable=False, doc="Der Testcasename wie im Testplan")
-    status = Column(sqlalchemy.Enum("_Status"),
+    status = Column(sqlalchemy.Enum(*stati),
                     doc="Ob der Testcase bereits implementiert/released wurde oder ob dies noch ein TODO ist")
     gid = Column(Integer, ForeignKey(Gruppe.__tablename__ + ".gid"))
     parameters = relationship("TestcaseParameter", collection_class=ordering_list('num'))
--- vba-ersatz.py
+++ vba-ersatz.py
@@ -2,28 +2,23 @@
 # -*- coding: utf-8 -*-
 import flask
 from flask import Flask, make_response
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse, request
 
 import DataModel
 
-app = None
-
-
-def createApp():
-    app = Flask(__name__)
-    return app
-
+app = Flask(__name__)
 
 def createApi():
     api = Api(app)
     api.add_resource(Homepage, '/', "/index.html")
     api.add_resource(About, '/about', "/about.html")
+    api.add_resource(Gruppe, "/gruppe/<int:nummer>", "/gruppe/<int:gid>/<string:command>")
     return api
 
 
-def render_template(name, *args, **kwargs):
+def render_template(name, **kwargs):
     """Rendert das Template und versucht den Content-Type zu erraten. Dies wird benötigt, da flask-restful das nicht selber kann."""
-    tpl = flask.render_template("index.html", *args, **kwargs)
+    tpl = flask.render_template(name, **kwargs)
     response = make_response(tpl)
     if name.endswith(".html"):
         response.headers['Content-Type'] = 'text/html'
@@ -35,12 +30,42 @@ def render_template(name, *args, **kwargs):
         response.headers["Content-Type"] = "text/plain"
     return response
 
-
 class Homepage(Resource):
     def get(self):
         result = render_template("index.html")
         return result
 
+
+class Gruppe(Resource):
+    def get(self, nummer, command="edit"):
+        app.logger.debug("GET: %s gruppe %d" % (command, nummer))
+        app.logger.debug("header: %s" % request.headers.get("Accept"))
+
+        gruppe = db_session.query(DataModel.Gruppe).filter(DataModel.Gruppe.nummer == nummer).first()
+        if gruppe is not None:
+            if "/html" in request.headers["Accept"]:
+                result = render_template("gruppe.html", gruppe=gruppe)
+            elif "/json" in request.headers["Accept"]:
+                result = render_template("gruppe.json", gruppe=gruppe)
+            else:
+                result = "Inkompatibler Accept-Header: %s" % request.headers["Accept"]
+            return result
+        else:
+            return "Gruppe %d existiert nicht" % nummer, 404
+
+    def post(self, nummer, command="edit"):
+        app.logger.debug("POST: %s gruppe %d" % (command, nummer))
+
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', type=str, help='Name der Gruppe')
+        parser.add_argument('status', type=str, help='Status der Gruppe')
+        args = parser.parse_args()
+
+        neueGruppe = DataModel.Gruppe(nummer=nummer, name=args["name"], status=args["status"])
+        db_session.add(neueGruppe)
+        db_session.commit()
+
+        return "OK"
 
 class About(Resource):
     def get(self):
@@ -49,7 +74,6 @@ class About(Resource):
         return result
 
 if __name__ == "__main__":
-    app = createApp()
     api = createApi()
     db_engine, db_session = DataModel.createDB(app)
     app.run(debug=True)
